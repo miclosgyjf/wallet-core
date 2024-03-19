1fb7bb0383b1c98670a23e9974958c7fa8aa2bfd
Satoshi Otomakan <satoshiotomakan8@gmail.com>
2024-03-19 18:10:10 +0100

    [PoC]: Add WalletModules to the Android main target

--- android/app/src/main/java/com/trustwallet/core/app/MainActivity.kt
+++ android/app/src/main/java/com/trustwallet/core/app/MainActivity.kt
@@ -2,10 +2,12 @@ package com.trustwallet.core.app
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import uniffi.wallet_modules.twExampleModuleRequest
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
+        twExampleModuleRequest(1u)
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
     }
